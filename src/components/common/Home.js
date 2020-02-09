0c062ac6973568dccfc4fec179334288df83e28c
Elyse Holladay <elyseholladay@gmail.com>
2020-02-09 00:18:44 -0600

    actually commit the home component, i should go to bed

---
+++ src/components/common/Home.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import { Link } from 'gatsby'
+
+// const Home = ({ pageContext }) => {
+const Home = () => {
+
+    return (
+        <div className="home">
+            I’m Elyse, and I’m a personal style coach. I help people transform their everyday relationship with clothes, break free of negative body image and shopping habits, and uncover their singular style.
+        </div>
+    )
+}
+
+// Home.propTypes = {
+//     pageContext: PropTypes.object.isRequired,
+// }
+
+export default Home
