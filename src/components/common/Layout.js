76f581ad44423857df98c2bd586a3e87d61585dd
Elyse Holladay <elyseholladay@gmail.com>
2020-02-08 18:53:27 -0600

    dont be stupid and leave JS code that doesnt work lying around in other files

--- src/components/common/Layout.js
+++ src/components/common/Layout.js
@@ -24,16 +24,6 @@ const DefaultLayout = ({ data, children, bodyClass, isHome }) => {
     const twitterUrl = site.twitter ? `https://twitter.com/${site.twitter.replace(/^@/, ``)}` : null
     const facebookUrl = site.facebook ? `https://www.facebook.com/${site.facebook.replace(/^\//, ``)}` : null
 
-    var images = document.querySelectorAll('.kg-gallery-image img');
-
-    images.forEach(function (image) {
-        var container = image.closest('.kg-gallery-image');
-        var width = image.attributes.width.value;
-        var height = image.attributes.height.value;
-        var ratio = width / height;
-        container.style.flex = ratio + ' 1 0%';
-    })  
-
     return (
         <>
             <Helmet>
--- src/components/common/useGalleryResize.js
+++ src/components/common/useGalleryResize.js
@@ -1,15 +1,17 @@
 import React, { useEffect } from 'react'
 
 function useGalleryResize(html) {
-        useEffect(() => {
-        var images = document.querySelectorAll('.kg-gallery-image img');
-        images.forEach(function (image) {
-            var container = image.closest('.kg-gallery-image');
-            var width = image.attributes.width.value;
-            var height = image.attributes.height.value;
-            var ratio = width / height;
-            container.style.flex = ratio + ' 1 0%';
-        })  
+    useEffect(() => {
+        if (typeof document !== 'undefined') {
+            var images = document.querySelectorAll('.kg-gallery-image img');
+            images.forEach(function (image) {
+                var container = image.closest('.kg-gallery-image');
+                var width = image.attributes.width.value;
+                var height = image.attributes.height.value;
+                var ratio = width / height;
+                container.style.flex = ratio + ' 1 0%';
+            })  
+        }
     }, [html])
 }
 
