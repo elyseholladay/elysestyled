6ddbff9379259cb7d5463b4d84a0d6248095817a
Elyse Holladay <elyseholladay@gmail.com>
2020-07-02 21:28:50 -0500

    remove shape til you can do it right

--- src/pages/index.js
+++ src/pages/index.js
@@ -12,11 +12,11 @@ const IndexPage = ({ data }) => {
         <Layout>
             <div className="home">
                 <div className="highlight">
-                    <div className="highlight-shape">
+                    {/* <div className="highlight-shape">
                         <svg xmlns="http://www.w3.org/2000/svg" width="276" height="284" viewBox="0 0 138.274 142.358">
                             <path id="Path_58" data-name="Path 58" d="M4.284,62.89C-.24,55.52-1.032,46.2,1.207,37.843S8.514,22.075,14.79,16.124A58.129,58.129,0,0,1,53.649,0c14.356-.139,28.8,5.872,38.316,16.625,9.45,10.681,13.652,25.4,13.034,39.652S99.242,84.316,91.921,96.555c-3,5.015-6.561,9.984-11.7,12.758-5.907,3.186-12.989,3.014-19.692,2.664-9.178-.478-18.488-1.164-27.126-4.3S16.749,98.6,13.076,90.179c-3.788-8.69-2.8-19.39-8.615-26.875C4.461,63.3,6.59,66.647,4.284,62.89Z" transform="translate(138.274 104.709) rotate(159)" fill="#a5bdc5" opacity="0.5"/>
                        </svg>
-                 </div>
+                    </div> */}
 
                     <div className="highlight-text">
                         are you ready to turn your wardrobe dreams into actions, commit to a more socially responsible wardrobe, & uncover your singular style?
